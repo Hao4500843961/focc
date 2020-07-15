16d0e0f993c8b5b168642932051279407df594d5
haoxl <450843961@qq.com>
2020-07-15 17:07:20 +0800

    modify by haoxl

--- src/main.js
+++ src/main.js
@@ -4,7 +4,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+/*add by haoxl*/
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+/*是否导入图标插件组建*/
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+/*add by haoxl*/
+
 Vue.config.productionTip = false
+/*add by haoxl*/
+Vue.use(BootstrapVue)
+/*是否安装bootstrapVue图标插件组建*/
+Vue.use(IconsPlugin)
+/*add by haoxl*/
 
 /* eslint-disable no-new */
 new Vue({
