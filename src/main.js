3d2ac4cde205dd6565fd96c8c58485e2ae4320de
haoxl <450843961@qq.com>
2020-07-24 15:05:24 +0800

    modify by haoxl 引入element-ui

--- src/main.js
+++ src/main.js
@@ -5,19 +5,21 @@ import App from './App'
 import router from './router'
 
 /*add by haoxl*/
-import 'bootstrap/dist/css/bootstrap.css'
+/*import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-/*是否导入图标插件组建*/
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
+/!*是否导入图标插件组建*!/
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'*/
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 /*add by haoxl*/
 
 Vue.config.productionTip = false
-/*add by haoxl*/
-Vue.use(BootstrapVue)
+/*add by haoxl bootstrap-vue*/
+//Vue.use(BootstrapVue)
 /*是否安装bootstrapVue图标插件组建*/
-Vue.use(IconsPlugin)
-/*add by haoxl*/
+//Vue.use(IconsPlugin)
+/*add by haoxl bootstrap-vue*/
+Vue.use(Element)
 
 /* eslint-disable no-new */
 new Vue({
