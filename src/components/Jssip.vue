805bf34586eb01eb626180f38e2d6d87a1cc6253
ttyang <578901938@qq.com>
2020-08-14 09:32:01 +0800

    主要login模块，还存在问题jssip is not defined

---
+++ src/components/Jssip.vue
@@ -0,0 +1,24 @@
+<script>
+  // const account = "1009";
+  // const password = "1234";
+  // const proxy = "ws://192.168.27.135:5066";
+
+  export default {
+    // account,
+    // password,
+    // proxy,
+
+    //注册
+    zhuce(acc,pass,p) {
+      var socket = new jssip.WebSocketInterface(p);
+      var configuration = {
+        sockets: [socket],
+        outbound_proxy_set: acc,
+        uri: this.account,
+        password: pass,
+        register: true,
+        session_timers: false,
+      };
+    },
+  };
+</script>
